2de2ace1760530651466ae8fe1d54f337ed42811
Sohyun <so405@naver.com>
2023-10-24 16:55:59 +0900

    [FEAT] : music 없을 때 처리 / #7

--- app/src/main/java/org/sopt/dosopttemplate/features/home/viewholder/FriendProfileViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/features/home/viewholder/FriendProfileViewHolder.kt
@@ -1,5 +1,6 @@
 package org.sopt.dosopttemplate.features.home.viewholder
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.dosopttemplate.databinding.ItemHomeFriendProfileBinding
 import org.sopt.dosopttemplate.features.home.model.Profile
@@ -8,6 +9,8 @@ class FriendProfileViewHolder(private val binding: ItemHomeFriendProfileBinding)
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(data: Profile.Friend) {
         with(binding) {
+            if (data.melonMusic.isNullOrEmpty()) tvItemHomeFriendProfileMelonMusic.visibility =
+                View.GONE
             profile = data
             executePendingBindings()
         }
