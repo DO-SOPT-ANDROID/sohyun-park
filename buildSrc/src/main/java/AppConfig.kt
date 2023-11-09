6ef809b2d96c97786a1ad0c4920848b1358b3f0a
Sohyun <so405@naver.com>
2023-11-10 03:10:17 +0900

    [UI] : Carousel ui 구성 / #11

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,7 +1,7 @@
 object AppConfig {
     const val PACKAGE_NAME = "org.sopt.dosopttemplate"
-    const val COMPILE_SDK = 33
-    const val TARGET_SDK = 33
+    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 34
     const val MIN_SDK = 28
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
