f1d281f25e79f8ec544b8b32fbee74de0ecc456f
Sohyun <so405@naver.com>
2023-11-15 23:02:29 +0900

    [FEAT] : retrofit di / #16

---
+++ app/src/main/java/org/sopt/dosopttemplate/di/RetrofitModule.kt
@@ -0,0 +1,45 @@
+package org.sopt.dosopttemplate.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.dosopttemplate.BuildConfig.BASE_URL
+import retrofit2.Retrofit
+import timber.log.Timber
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): Interceptor {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            Timber.tag("Retrofit2").d("CONNECTION INFO -> %s", message)
+        }
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: Interceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+
+    @Singleton
+    @Provides
+    @DoSoptRetrofit
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .client(okHttpClient)
+        .build()
+}
