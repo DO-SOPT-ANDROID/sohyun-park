5bbe46c50b9f995923d77b9b3da53daa7b94896c
Sohyun <so405@naver.com>
2023-12-06 14:51:10 +0900

    [FIX] : 상수화 코리 반영 / #20

--- app/src/main/java/org/sopt/dosopttemplate/data_remote/api/ReqresApiService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data_remote/api/ReqresApiService.kt
@@ -2,13 +2,13 @@ package org.sopt.dosopttemplate.data_remote.api
 
 import org.sopt.dosopttemplate.data.dto.BaseResponseNullable
 import org.sopt.dosopttemplate.data.dto.response.ResponseListUsers
+import org.sopt.dosopttemplate.data_remote.api.AuthApiService.Companion.API
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ReqresApiService {
 
     companion object {
-        const val API = "api"
         const val USERS = "users"
         const val PAGE = "page"
     }
